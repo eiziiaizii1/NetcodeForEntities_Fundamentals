342dad7e3e38d6218a56caf73454df4e437587a6
Aziz Önder <azizonder05@gmail.com>
2026-02-08 23:32:55 +0300

    .
    Sending and Receiving RPC
    Client Build and PlayMode Tools
    Multiplayer Play Mode Package
    Setup Connection as InGame
    Netcode Ghosts, Player Object
    Player Movement, IInputComponentData
    Client Prediction with Lag

---
+++ Assets/_Scripts/PlayerAuthoring.cs
@@ -0,0 +1,19 @@
+using Unity.Entities;
+using UnityEngine;
+
+public class PlayerAuthoring : MonoBehaviour
+{
+    private class Baker : Baker<PlayerAuthoring>
+    {
+        public override void Bake(PlayerAuthoring playerAuthoring)
+        {
+            Entity entity = GetEntity(TransformUsageFlags.Dynamic);
+            AddComponent(entity, new Player());
+        }
+    }
+}
+
+public struct Player : IComponentData
+{
+
+}
